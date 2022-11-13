9655c986725aecd0c8e2ad39d89539b9330f04e2
natuvangard <ryoryoryovangard@gmail.com>
2022-11-13 10:19:55 +0900

    flutter_tr

---
+++ lib/timeline.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class timeline extends StatefulWidget {
+  static const String id = 'timeline';
+
+
+  const timeline({super.key, required this.title});
+  final String title;
+
+  @override
+  State<timeline> createState() => _timeline();
+}
+class _timeline extends State<timeline> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF1ECE4),
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: const Color(0xFF273668),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        //currentIndex: ,
+        //onTap: _onItemTapped,
+        //labelStyle: TextStyle(color: /*ラベルの色*/,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home,color:Colors.white), label: 'ホーム',),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite,color:Colors.white), label: 'お気に入り'),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications,color:Colors.white), label: 'お知らせ'),
+          BottomNavigationBarItem(icon: Icon(Icons.person,color:Colors.white), label: 'アカウント'),
+        ],
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF273668),
+      ),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              SizedBox(
+                height: 60.0,
+                width: 150.0,
+              ),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+              contentflow(),
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+}
+
+class contentflow extends StatelessWidget {
+  const contentflow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      child: Card(
+        color: Colors.white,
+        margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: CircleAvatar(
+                      radius: 40.0,
+                      backgroundImage: AssetImage('images/6652.jpeg'),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('デンジさんが敬語の勉強を完了',
+                        textAlign: TextAlign.center,
+                      ),
+                      Row(
+                        //mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          TextButton(
+                            child: const Text('フォロー！'),
+                            onPressed: (){
+                              //Navigator.pushNamed(context, Demo_bene.id);
+                            },),
+                          const SizedBox(width: 8),
+                          TextButton(
+                            child: const Text('進捗状況'),
+                            onPressed: () {/* ... */},
+                          ),
+                          const SizedBox(width: 8),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
