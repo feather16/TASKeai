633af7ea58462613975d7823cad28e600d1262c4
feather16 <hadarion721@gmail.com>
2022-11-13 12:46:40 +0900

    画面遷移

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:todo1/todo.dart';
+import 'package:todo1/timeline.dart';
+import 'package:todo1/mypage.dart';
+import 'package:todo1/task_edit.dart';
 
 class todo extends StatelessWidget {
   const todo({Key? key}) : super(key: key);
@@ -25,6 +30,33 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: const Text('ToDoList'),
           backgroundColor: const Color(0xFF388E3C),
+        ),
+        bottomNavigationBar: ConvexAppBar(
+          items: [
+            TabItem(icon: Icons.home, title: 'Home'),
+            TabItem(icon: Icons.map, title: 'Discovery'),
+            TabItem(icon: Icons.add, title: 'Add'),
+            TabItem(icon: Icons.message, title: 'Message'),
+            TabItem(icon: Icons.people, title: 'Profile'),
+          ],
+          backgroundColor: const Color(0xFF273668),
+          onTap: (int i) => {
+            if(i == 1){
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => timeline(title: 'タイムライン')
+              ))
+            }
+            else if(i == 2){
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => task_edit()
+              ))
+            }
+            else if(i == 4){
+              Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => mypage(title: 'マイページ')
+              ))
+            }
+          },
         ),
         body: ListView.builder(
           itemCount: _todos.length,
